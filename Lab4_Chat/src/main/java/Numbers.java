8e63946f24582ec716a65bf357125ef88d943f32
lmacbride <L00177577@atu.ie>
2023-03-28 17:57:55 +0100

    committing updates

---
+++ Lab4_Chat/src/main/java/Numbers.java
@@ -0,0 +1,27 @@
+
+public class Numbers {
+	public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+
+        // addition
+        int sum = num1 + num2;
+        System.out.println("Sum of " + num1 + " and " + num2 + " is " + sum);
+
+        // subtraction
+        int diff = num1 - num2;
+        System.out.println("Difference between " + num1 + " and " + num2 + " is " + diff);
+
+        // multiplication
+        int product = num1 * num2;
+        System.out.println("Product of " + num1 + " and " + num2 + " is " + product);
+
+        // division
+        int quotient = num1 / num2;
+        System.out.println("Quotient of " + num1 + " and " + num2 + " is " + quotient);
+
+        // modulus
+        int remainder = num1 % num2;
+        System.out.println("Remainder of " + num1 + " and " + num2 + " is " + remainder);
+    }
+}
